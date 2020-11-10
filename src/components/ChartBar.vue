80c50849e3857a3649f1f193d855700e20b4f00a
Robson Oliveira <robson@solutiontrue.com.br>
2020-11-10 10:25:50 -0300

    Initial commit

---
+++ src/components/ChartBar.vue
@@ -0,0 +1,17 @@
+<script>
+/*
+  ATENÇÃO!!! Não incluir a tag template neste wrapper
+  Consulta https://vue-chartjs.org/guide/#vue-single-file-components para mais informações
+
+*/
+// O import aqui pode ser  Line, Bar, Radar, Doughnut & Pie, Polar Area, Bubble, Scatter, Area e Mixed 
+// consultar https://www.chartjs.org/docs/latest/charts/ para mais informações
+import { Bar } from 'vue-chartjs' 
+export default {
+  extends: Bar,
+  props: ['chartdata', 'options'],
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+  }
+}
+</script>
